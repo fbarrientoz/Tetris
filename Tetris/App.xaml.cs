a6016b2be19137ca535c4d190a85ac7424bace3c
fbarrientos <fbarrientos@uabc.edu.mx>
2021-04-08 19:37:21 -0700

    Declare variables y agrege recursos multimedia al proyecto

--- Tetris/App.xaml.cs
+++ Tetris/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Tetris
 {
@@ -13,5 +14,22 @@ namespace Tetris
     /// </summary>
     public partial class App : Application
     {
+        //Declaracion de Variables
+            //Lista para agregar 2 sonidos .wav
+            List<System.Media.SoundPlayer> soundList = new List<System.Media.SoundPlayer>();
+        //DispatcherTime
+        DispatcherTimer timer;
+        //Figuras Random
+        Random shapeRandom;
+
+
+        //Reproducir el sonido. Index = 0 va reproducir collided.wav y Index = 1 Reproducri Delete Line
+        private void playSound(int index)
+        {
+            soundList[index].Play();
+        }
     }
+
+
+
 }
